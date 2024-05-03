ac92f764a9ff4f5b0546c19a88790774c77e8d77
nupsterd <cdanielmolina@gmail.com>
2024-05-03 17:52:10 -0500

    Primer script 01_introduccion

---
+++ 01_introduccion.py
@@ -0,0 +1,21 @@
+# Esto es un comentario:
+# El famoso Hola mundo en Python
+print("Hola Mundo - Python")
+
+""""
+Este es un comentario  diferente al numeral
+Lo que se puede lograr es poner mas información
+entre lineas sin necesidad de dar inicio en cada una
+con un numeral.
+"""
+
+'''
+Otra forma de hacer comentarios 
+en varias lineas
+'''
+# Consultar el tipo de dato
+print(type("Hola mundo")) # string
+print(type(2)) # int
+print(type(1.5)) # float
+print(type(True)) # bool
+print(type(1 + 1j)) # complex
