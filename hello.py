656df60a5eaf55dc0abd091848d20492a7979edd
nupsterd <cdanielmolina@gmail.com>
2024-05-03 15:58:37 -0500

    Inicio con Python y comentarios

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("Hola mundo")
+# Esto es un comentario:
+# El famoso Hola mundo en Python
+print("Hola Mundo - Python")
+
+""""
+Este es un comentario  diferente al numeral
+Lo que se puede lograr es poner mas información
+entre lineas sin necesidad de dar inicio en cada una
+con un numeral.
+"""
+
+'''
+Este es un comentario  diferente al numeral
+Lo que se puede lograr es poner mas información
+entre lineas sin necesidad de dar inicio en cada una
+con un numeral.
+'''
